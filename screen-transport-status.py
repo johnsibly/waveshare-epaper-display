231e1e8b438571b533b6ad72cdac09208e35a626
John Sibly <john@sibly.co.uk>
2020-03-08 12:23:18 +0000

    Removed unused bus stop code

--- screen-transport-status.py
+++ screen-transport-status.py
@@ -7,32 +7,10 @@ template = 'screen-output-weather.svg'
 
 busUrl = 'https://api.tfl.gov.uk/Journey/JourneyResults/sw152qa/to/W67AP?nationalSearch=false&Mode=bus&timeIs=Departing&journeyPreference=LeastTime'
 tubeUrl = 'https://api.tfl.gov.uk/Journey/JourneyResults/sw152qa/to/W67AP?nationalSearch=false&Mode=tube&timeIs=Departing&journeyPreference=LeastTime'
-busArrivals = 'https://api.tfl.gov.uk/StopPoint/490006048W/arrivals'
 tubeStatusUrl = 'https://api.tfl.gov.uk/line/mode/tube/status'
 
-bus1 = ''
-bus2 = ''
-bus3 = ''
 tubeTime = 'Undefined'
 busTime = 'Undefined'
-
-def wheresMyBus():
-    global bus1, bus2, bus3
-    json = requests.get(busArrivals).json()
-    sortedBuses = sorted(json, key=lambda a: a['timeToStation']) 
-
-    if len(sortedBuses) > 0:
-        bus = sortedBuses[0]
-        bus1 = bus['lineName'] + ' in ' + str(int(bus['timeToStation']/60)) + ' minutes'
-        if len(sortedBuses) > 1:
-            bus = sortedBuses[1]
-            bus2 = bus['lineName'] + ' in ' + str(int(bus['timeToStation']/60)) + ' minutes'
-            if len(sortedBuses) > 2:
-                bus = sortedBuses[2]
-                bus3 = bus['lineName'] + ' in ' + str(int(bus['timeToStation']/60)) + ' minutes'
-    print(bus1)
-    print(bus2)
-    print(bus3)
     
 def howIsJourney(tuneUrl, option):
     json = requests.get(tuneUrl).json()
@@ -57,13 +35,9 @@ def howIsDistrict():
 
 busTime = howIsJourney(busUrl, 'bus')
 tubeTime = howIsJourney(tubeUrl, 'tube')
-wheresMyBus()
 
 # Process the SVG
 output = codecs.open(template , 'r', encoding='utf-8').read()
 output = output.replace('OPTION_TUBE', tubeTime.capitalize())
 output = output.replace('OPTION_BUS', busTime.capitalize())
-output = output.replace('BUS_1', bus1)
-output = output.replace('BUS_2', bus2)
-output = output.replace('BUS_3', bus3)
 codecs.open('screen-output-weather.svg', 'w', encoding='utf-8').write(output)
