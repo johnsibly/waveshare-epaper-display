8256287b9813cd6a392ae7a19d6da0495bd756fc
John Sibly <john@sibly.co.uk>
2020-03-29 09:56:28 +0100

    Fixed bug with datetime with timezone offset

--- screen-calendar-get.py
+++ screen-calendar-get.py
@@ -71,6 +71,9 @@ for x in range(5):
         if len(start) == 10:
             day = time.strftime("%a %b %d",time.strptime(start,"%Y-%m-%d"))
         else:
+            if len(start) > 19:
+                # Stripout timezone offset, e.g. make 2020-03-30T18:45:00+01:00 => 2020-03-30T18:45:00
+                start = start[0:19]
             day = time.strftime("%a %b %d %X",time.strptime(start,"%Y-%m-%dT%H:%M:%S%fZ"))
         desc = event['summary']
         print(day, desc)
